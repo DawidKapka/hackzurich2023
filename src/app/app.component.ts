0a9d4c221f06ba23cdc365183798ad5559fb483b
Leo Scherer <lenoxy@gmx.ch>
2023-09-16 02:26:03 +0200

    Add opentransport api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {format, formatDistance, formatRelative, parseISO, subDays} from 'date-fns'
+import {HttpClient} from "@angular/common/http";
+import {da} from "date-fns/locale";
+
+
 
 @Component({
   selector: 'app-root',
@@ -10,11 +15,40 @@ export class AppComponent implements AfterViewInit {
 
   @ViewChild('googleMaps') googleMaps: ElementRef | undefined;
 
+  public pointA: string | undefined;
+  public pointB: string | undefined;
+  public dateTimeLocal: string = format(new Date(), 'yyyy-MM-dd\'T\'HH:mm');
+
   ngAfterViewInit() {
     this.initGoogleMaps()
+    console.log(format(new Date(), 'yyyy-MM-dd\'T\'HH:mm'))
   }
 
   private initGoogleMaps() {
 
   }
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+
+  getConnections(pointA: string | undefined, pointB: string | undefined, dateTimeLocal: string) {
+    if(!pointA || !pointB){
+      console.error("input empty")
+      return
+    }
+    this.getPublicTransportConnections(pointA!, pointB!, parseISO(dateTimeLocal))
+
+  }
+
+  getPublicTransportConnections(pointA: string, pointB: string, dateTimeLocal: Date) {
+    const date = format(dateTimeLocal, 'yyyy-MM-dd');
+    const time = format(dateTimeLocal, 'HH:mm')
+    this.httpClient.get(`http://transport.opendata.ch/v1/connections?from=${pointA}&to=${pointB}&date=${date}&time=${time}`)
+      .subscribe((data: any) =>  {
+        // TODO process data
+        console.log(data)
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {GoogleMapsModule} from "@angular/google-maps";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import {GoogleMapsModule} from "@angular/google-maps";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    GoogleMapsModule
+    GoogleMapsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
